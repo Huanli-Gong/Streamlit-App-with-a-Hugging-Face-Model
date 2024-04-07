0ac452c77533f42b8eda6428d2ce3e6ce6d590f2
Huanli-Gong <huanli0125@outlook.com>
2024-04-07 13:10:40 -0400

    Initial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,23 @@
 import streamlit as st
-from transformers import pipeline, set_seed
+from transformers import pipeline
 
-st.title("Inference API: openai-gpt")
-text_input = st.text_area("Text Generation:", value="Hello, I'm a language model,")
-generator = pipeline('text-generation', model='openai-gpt')
-set_seed(42)
+# Load the language model
+model = pipeline("text-generation", model="openai-gpt")
 
-if st.button("Generate"):
-    if text_input:
-        for output in generator(text_input, do_sample=True, max_length=30):
-            st.write(output)
-    else:
-        st.warning("Please enter text first.")
+
+# Define Streamlit app
+def main():
+    st.title("Language Model Deployment with Streamlit")
+    text_input = st.text_area("Enter text to generate continuation:")
+
+    if st.button("Generate"):
+        if text_input:
+            generated_text = model(text_input, max_length=50, do_sample=True)[0]['generated_text']
+            st.write("Generated Text:")
+            st.write(generated_text)
+        else:
+            st.warning("Please enter some text first.")
+
+
+if __name__ == "__main__":
+    main()
